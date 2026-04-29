def908f86e35903faba12cdb19256de4b2293505
emilceflores <emilce.flores@ucb.edu.bo>
2026-04-28 22:59:33 -0400

    Add files via upload

--- PRACTICA_09/Ejercicio_09_04.cpp
+++ PRACTICA_09/Ejercicio_09_04.cpp
@@ -10,4 +10,36 @@ Sea n=5
 5 6 7 8 9
 7 8 9 10 11
 9 10 11 12 13*/
- 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Ingrese n: ";
+    cin >> n;
+
+    vector<vector<int>> matriz(n, vector<int>(n));
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            matriz[i][j] = (2*i + 1) + j;
+        }
+    }
+
+    cout << "\nMatriz generada:\n";
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
